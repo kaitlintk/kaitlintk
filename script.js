ba26dc73254cb24a1c0b31320d6f5d54805e9616
Kaitlin <kaitlin@169-231-134-80.wireless.ucsb.edu>
2022-11-05 14:29:16 -0700

    initial commit'

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    // var icon = document.getElementById("icon"); 
+    
+    //icon.addEventListener("click", function() {
+        //icon.style.width = "100px";
+    //};
+
+    var icon = $("#icon"); 
+
+    icon.hover(function() {
+        alert( "Handler for .hover() called." );
+      });
+
+
+}
+
+
+
+
+
+
+    // event listeners
+    // backend/frontend calls
